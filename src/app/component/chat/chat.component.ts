d91d0b1bbc3255e563ed68d7c7c7c21525695e7d
David de los Reyes <dlos@minsait.com>
2024-04-23 10:53:48 +0200

    Connected to broker

--- src/app/component/chat/chat.component.ts
+++ src/app/component/chat/chat.component.ts
@@ -1,4 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Client, IStompSocket, IStompSocketMessageEvent } from '@stomp/stompjs';
+import SockJS from 'sockjs-client';
+
+interface SockJSStompSocket extends IStompSocket {
+  // Define las propiedades o métodos necesarios para que SockJS sea compatible
+  // Aquí definimos una propiedad onmessage que acepta un callback de tipo IStompSocketMessageEvent
+  onmessage: ((ev: IStompSocketMessageEvent) => any) | null;
+  // Otros métodos o propiedades necesarios para que SockJS sea compatible con IStompSocket
+}
 
 @Component({
   selector: 'app-chat',
@@ -7,6 +16,25 @@ import { Component } from '@angular/core';
   templateUrl: './chat.component.html',
   styleUrl: './chat.component.scss'
 })
-export class ChatComponent {
+
+export class ChatComponent implements OnInit{
+
+  private client!: Client;
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.client = new Client();
+
+    this.client.webSocketFactory = () => {
+      return new SockJS("http://localhost:8080/chat-websocket") as SockJSStompSocket;
+    };
+
+    this.client.onConnect = (frame) => {
+      console.log('Conectado: ' + this.client.connected + ' : ' + frame);
+    }
+    this.client.activate();
+  }
 
 }
+
